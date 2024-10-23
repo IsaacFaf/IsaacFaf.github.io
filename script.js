4419b8995efa6af7d86ff93f07cff67e7ab104aa
IsaacFaf <89608287+IsaacFaf@users.noreply.github.com>
2024-10-23 19:04:47 -0400

    noice

--- script.js
+++ script.js
@@ -39,3 +39,12 @@ videos.forEach((video, index) => {
 
 // Initialiser la première vidéo
 updateVideo()
+
+
+
+/* Parallax effect when scrolling */
+window.addEventListener('scroll', function() {
+    const parallaxImage = document.querySelector('.parallax-image');
+    let scrollPosition = window.pageYOffset;
+    parallaxImage.style.transform = 'translateY(' + scrollPosition * 0.3 + 'px)';
+  });
